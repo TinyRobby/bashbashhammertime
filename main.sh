19a23b975a1e3dc048b770cabdc5aa3d7eccdca1
Robby <>
2021-05-14 13:30:36 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,27 @@
+echo =+\\=+\\=+\\=+\\=+\\\n  
+echo \ \ \ \ \ A Game. 
+echo =+\\=+\\=+\\=+\\=+\\\n
+read main
+echo there is no Game
+sleep 2
+echo go away
+read ioi
+echo ok
+sleep 0.5
+echo here is a game!
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+read main
